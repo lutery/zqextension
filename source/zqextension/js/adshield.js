1fd815e3e638174d44b9e5d34da183af6b186334
ch4534 <453413516@qq.com>
2017-05-18 17:58:17 +0800

    增加
    myjs增加了监听

--- source/zqextension/js/adshield.js
+++ source/zqextension/js/adshield.js
@@ -1,6 +1,3 @@
 window.onload = function(){
-    //detail:message sender sendResponse
-    chrome.extension.onMessage.addListener(function(detail){
-
-    });
+    
 }
--- source/zqextension/js/myjs.js
+++ source/zqextension/js/myjs.js
@@ -26,6 +26,49 @@ window.onload = function(){
      $("#js-right-chat-panel .js-right-chat-layer").height(chatHeight);
      $("#js-right-chat-panel .slimScrollDiv").height(chatHeight);
      $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(chatHeight);
+
+     //detail:message sender sendResponse
+     // message(any):adshield efshield bbstyle
+     // adshield:{bShield:, LRCHeight:, fangsHeight:, controlHeight:, chatHeight:}
+    chrome.extension.onMessage.addListener(function(detail){
+         
+         if ('adshield' in detail.message){
+            var adshield = detail.message.adshield;
+
+            if (adshield.bShield){
+                 //获取直播区域的高度
+                var LRCHeight = $(".live-room-content").height();
+
+                //获取粉丝列表的高度
+                var fangsHeight = $("#js-right-chat-panel .js-fans-list-panel").height();
+
+                //获取聊天输入框的高度
+                var controlHeight = $("#js-chat-control-panel").height();
+
+                //计算聊天信息栏在隐藏广告后的高度
+                var chatHeight = LRCHeight - fangsHeight - controlHeight;
+
+                //配置聊天信息栏的新高度
+                $("#js-right-chat-panel .js-right-chat-layer").height(chatHeight);
+                $("#js-right-chat-panel .slimScrollDiv").height(chatHeight);
+                $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(chatHeight);
+            }
+            else{
+                //配置聊天信息栏的新高度
+                $("#js-right-chat-panel .js-right-chat-layer").height(adshield.chatHeight);
+                $("#js-right-chat-panel .slimScrollDiv").height(adshield.chatHeight);
+                $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(adshield.chatHeight);
+            }
+         }
+
+         if ('efshield' in detail.message){
+
+         }
+
+         if ('bbstyle' in detail.message){
+
+         }
+    });
 }
 
 // function checkMaxScrean(){
