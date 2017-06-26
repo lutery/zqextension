202d56756344826ccd1c475add0645eae484c415
lutery <lutery@foxmail.com>
2017-06-26 22:02:34 +0800

    修改

--- source/zqextension/js/myjs.js
+++ source/zqextension/js/myjs.js
@@ -1,16 +1,8 @@
 // alert(123);
 
-     chrome.extension.onRequest.addListener(
-         function(request, sender, sendResponse){
-            console.log("this is onRequest");
-            console.log(request);
-            console.log(sender);
-            console.log(sendResponse);
-         }
-     );
 //chat-flash-gg
 // $(".live-chat-content .chat-flash-gg").css('display', 'none');
-window.onload = function(){
+window.onload = function() {
     console.log("window.onload = function(){");
     // alert(123);
     //隐藏聊天框上部分广告
@@ -36,80 +28,73 @@ window.onload = function(){
     //  $("#js-right-chat-panel .slimScrollDiv").height(chatHeight);
     //  $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(chatHeight);
 
-     //detail:message sender sendResponse
-     // message(any):adshield efshield bbstyle
-     // adshield:{bShield:}
-    chrome.extension.onMessage.addListener(function(detail){
-        console.log("chrome.extension");
-         
-         if ('adshield' in detail.message){
-            var adshield = detail.message.adshield;
+    //detail:message sender sendResponse
+    // message(any):adshield efshield bbstyle
+    // adshield:{bShield:}
+    chrome.extension.onRequest.addListener(
+        function(request, sender, sendResponse) {
+            console.log("this is onRequest");
+            if ('adshield' in detail.message) {
+                var adshield = detail.message.adshield;
 
-            if (adshield.bShield){
-                $("body").addClass("zqadshield");
+                if (adshield.bShield) {
+                    $("body").addClass("zqadshield");
 
-                 //获取直播区域的高度
-                var LRCHeight = $(".live-room-content").height();
-
-                //获取粉丝列表的高度
-                var fangsHeight = $("#js-right-chat-panel .js-fans-list-panel").height();
-
-                //获取聊天输入框的高度
-                var controlHeight = $("#js-chat-control-panel").height();
-
-                //计算聊天信息栏在隐藏广告后的高度
-                var chatHeight = LRCHeight - fangsHeight - controlHeight;
-
-                                
-                $.cookie('LRCHeight', LRCHeight);
-                $.cookie('fangsHeight', fangsHeight);
-                $.cookie('controlHeight', controlHeight);
-                $.cookie('chatHeight', $("#js-right-chat-panel .js-right-chat-layer"));
-
-                //配置聊天信息栏的新高度
-                $("#js-right-chat-panel .js-right-chat-layer").height(chatHeight);
-                $("#js-right-chat-panel .slimScrollDiv").height(chatHeight);
-                $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(chatHeight);
-            }else{
-                $("body").removeClass("zqadshield");
-
-                var LRCHeight = $.cookie('LRCHeight');
-                var fangsHeight = $.cookie('fangsHeight');
-                var controlHeight = $.cookie('controlHeight');
-                var chatHeight = $.cookie('chatHeight');
-
-                if (LRCHeight != null){
                     //获取直播区域的高度
-                    $(".live-room-content").height(LRCHeight);
-                }
+                    var LRCHeight = $(".live-room-content").height();
 
-                if (fangsHeight != null){
                     //获取粉丝列表的高度
-                    $("#js-right-chat-panel .js-fans-list-panel").height(fangsHeight);
-                }
+                    var fangsHeight = $("#js-right-chat-panel .js-fans-list-panel").height();
 
-                if (controlHeight != null){
                     //获取聊天输入框的高度
-                    $("#js-chat-control-panel").height(controlHeight);
-                }
+                    var controlHeight = $("#js-chat-control-panel").height();
 
-                if (chatHeight != null){
+                    //计算聊天信息栏在隐藏广告后的高度
+                    var chatHeight = LRCHeight - fangsHeight - controlHeight;
+
+
+                    $.cookie('LRCHeight', LRCHeight);
+                    $.cookie('fangsHeight', fangsHeight);
+                    $.cookie('controlHeight', controlHeight);
+                    $.cookie('chatHeight', $("#js-right-chat-panel .js-right-chat-layer"));
+
                     //配置聊天信息栏的新高度
                     $("#js-right-chat-panel .js-right-chat-layer").height(chatHeight);
                     $("#js-right-chat-panel .slimScrollDiv").height(chatHeight);
                     $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(chatHeight);
+                } else {
+                    $("body").removeClass("zqadshield");
+
+                    var LRCHeight = $.cookie('LRCHeight');
+                    var fangsHeight = $.cookie('fangsHeight');
+                    var controlHeight = $.cookie('controlHeight');
+                    var chatHeight = $.cookie('chatHeight');
+
+                    if (LRCHeight != null) {
+                        //获取直播区域的高度
+                        $(".live-room-content").height(LRCHeight);
+                    }
+
+                    if (fangsHeight != null) {
+                        //获取粉丝列表的高度
+                        $("#js-right-chat-panel .js-fans-list-panel").height(fangsHeight);
+                    }
+
+                    if (controlHeight != null) {
+                        //获取聊天输入框的高度
+                        $("#js-chat-control-panel").height(controlHeight);
+                    }
+
+                    if (chatHeight != null) {
+                        //配置聊天信息栏的新高度
+                        $("#js-right-chat-panel .js-right-chat-layer").height(chatHeight);
+                        $("#js-right-chat-panel .slimScrollDiv").height(chatHeight);
+                        $("#js-right-chat-panel .slimScrollDiv .js-chat-msg-scroll").height(chatHeight);
+                    }
                 }
             }
-         }
-
-        //  if ('efshield' in detail.message){
-
-        //  }
-
-        //  if ('bbstyle' in detail.message){
-
-        //  }
-    });
+        }
+    );
 }
 
 // function checkMaxScrean(){
--- source/zqextension/js/popup.js
+++ source/zqextension/js/popup.js
@@ -6,17 +6,17 @@
 //     // chrome.runtime.connect("options_ui");
 // });
 
-window.onload = function(){
+window.onload = function() {
     // console.log("window onload");
-    
-    chrome.storage.local.get(["adshield", "efshield", "bbstyle"], function(items){
+
+    chrome.storage.local.get(["adshield", "efshield", "bbstyle"], function(items) {
         console.log(items);
         $("#adshield").attr("checked", items.adshield);
         $("#efshield").attr("checked", items.efshield);
         $("#bbstyle").attr("checked", items.bbstyle);
     });
 
-    $("#hot-word-item").on('click', function(){
+    $("#hot-word-item").on('click', function() {
         console.log("hot-word-item is click");
         // alert(123);
         if (chrome.runtime.openOptionsPage) {
@@ -28,35 +28,31 @@ window.onload = function(){
         }
     });
 
-   chrome.runtime.onMessage.addListener(function(msg, _, sendResponse){
-            console.log("chrome.extension.onMessage.addListener");
+    chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
+        console.log("chrome.extension.onMessage.addListener");
     });
 
-    $("#adshield").bind("click", function(){
+    $("#adshield").bind("click", function() {
         console.log("adshield click");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             var tab = tabs[0];
-            chrome.tabs.sendRequest(tab.id, {adshield: true}, function handler(response) {
+            chrome.tabs.sendRequest(tab.id, { adshield: true }, function handler(response) {
                 console.log("this is request");
             });
         });
         // console.log($("#adshield").is(":checked"));
-        chrome.storage.local.set({"adshield":$("#adshield").is(":checked")}, function(){
-        });
+        chrome.storage.local.set({ "adshield": $("#adshield").is(":checked") }, function() {});
 
-        chrome.runtime.sendMessage({adshield:true});
+        chrome.runtime.sendMessage({ adshield: true });
     });
 
-    $("#efshield").bind("click", function(){
+    $("#efshield").bind("click", function() {
         console.log("efshield click");
-        chrome.storage.local.set({"efshield":$('#efshield').is(":checked")}, function(){
-        });
+        chrome.storage.local.set({ "efshield": $('#efshield').is(":checked") }, function() {});
     });
 
-    $("#bbstyle").bind("click", function(){
+    $("#bbstyle").bind("click", function() {
         console.log("bbstyle click");
-        chrome.storage.local.set({"bbstyle":$('#bbstyle').is(":checked")}, function(){
-        });
+        chrome.storage.local.set({ "bbstyle": $('#bbstyle').is(":checked") }, function() {});
     });
 }
-
